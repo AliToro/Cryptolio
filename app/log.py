7fe459fae7a4c403aba2bd39857a506f2b44fb8d
Alireza Haghighat <alireza.haghighat@zalando.de>
2022-02-05 20:47:15 +0100

    Change files to run on Docker

--- app/log.py
+++ app/log.py
@@ -1,4 +1,4 @@
 import logging
 
-logging.basicConfig(filename='logs/cryptolio.log', format='%(asctime)s %(levelname)-8s %(message)s',
+logging.basicConfig(filename='./cryptolio.log', format='%(asctime)s %(levelname)-8s %(message)s',
                     level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
